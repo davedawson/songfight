f5cf151dbb0f2879483fa6ff3d659035c7f22613
davedawson <dave@iamdavedawson.com>
2012-12-30 15:25:56 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songfight::Application.config.secret_token = '6c556ea3849a2bd9fc72d5150834682cebbbb3607900c2db13d316c6b2726e783e2ec6138947b2be2740688780661ce35162ba9ea49188ef63836e9749d46340'
